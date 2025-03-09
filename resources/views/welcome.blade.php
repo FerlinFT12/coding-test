65d9b3378dc41d14e39b10f51dc31536d6b32612
ferlinft12 <ferlinft@gmail.com>
2025-03-10 00:17:31 +0700

    update kode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
                     <div class="grid grid-cols-12 md:grid-cols-12 gap-6 lg:gap-12">
                         <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Coding Test</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Coding Test Hashmicro</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Nama Kandidat : Ferlin Firdaus Turnip<br>
